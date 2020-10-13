6f02c26825fc55a43d742ef538324f4397519b6e
Jefferson Euclides <jefferson.euclides.santos@sap.com>
2020-10-12 22:56:57 -0300

    Solve empty folders bug

--- src/main/java/com/teste/ilegra/ilegra/service/file/FileMove.java
+++ src/main/java/com/teste/ilegra/ilegra/service/file/FileMove.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Component
 @Slf4j
@@ -17,7 +18,7 @@ public class FileMove {
 
     private void move(String src, String dest ) {
         try {
-            Files.move(Paths.get(src), Paths.get(dest));
+            Files.move(Paths.get(src), Paths.get(dest), StandardCopyOption.REPLACE_EXISTING);
             log.info("File moved to the Processed folder");
         } catch (IOException e) {
             log.error("Exception while moving file: " + e.getMessage());
